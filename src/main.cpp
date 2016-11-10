7f01593f8d9a0ec83c5903014888d36d9c00cc07
Wojciech Gruszka <wojciech@gruszka.eu>
2016-11-10 09:37:30 +0100

    Changed benchmarks to work with c++11

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include <cstdlib>
 #include <string>
 #include <random>
-#include "../CODEine/benchmark.h"
 #include "Vector.h"
 #include "LinkedList.h"
 #include <iostream>
@@ -67,46 +66,29 @@ void randomInsert(int pCount) {
 int main(int argc, char** argv) {
     (void) argc;
     (void) argv;
-    //prepend<Vector<int>>(12222);
-    //append<Vector<int>>(12222);
-    //prepend<List<int>>(12222);
-    //append<List<int>>(12222);
 
-/*    std::cout << "Append" << std::endl;
+    auto start = clock();
+    prepend<Vector<int>>(100000);
+    auto end = clock();
+    std::cout << "Prepend Vector: " << ((float) (end - start)) / CLOCKS_PER_SEC << std::endl;
 
-    bmk::benchmark<> append_bm;
-    append_bm.run("Vector", 10, append<Vector<int>>, "Elements",
-                  {100, 1000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000});
-    append_bm.run("List", 10, append<List<int>>, "Elements",
-                  {100, 1000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000});
-    append_bm.serialize("Append", "append.txt");
 
-    std::cout << "Prepend" << std::endl;
+    start = clock();
+    prepend<List<int>>(100000);
+    end = clock();
+    std::cout << "Prepend List: " << ((float) (end - start)) / CLOCKS_PER_SEC << std::endl;
 
-    bmk::benchmark<> prepend_bm;
-    prepend_bm.run("Vector", 1, prepend<Vector<int>>, "Elements",
-                   {100, 1000, 10000, 50000, 100000, 500000, 1000000});
-    prepend_bm.run("List", 1, prepend<List<int>>, "Elements",
-                   {100, 1000, 10000, 50000, 100000, 500000, 1000000});
-    prepend_bm.serialize("Prepend", "prepend.txt");
+    start = clock();
+    randomInsert<Vector<int>>(100000);
+    end = clock();
+    std::cout << "RandomInsert Vector: " << ((float) (end - start)) / CLOCKS_PER_SEC << std::endl;
 
-    std::cout << "PopFirst" << std::endl;
 
-    bmk::benchmark<> pop_first_bm;
-    pop_first_bm.run("Vector", 1, popFirst<Vector<int>>, "Elements",
-                     {100, 1000, 10000, 50000, 100000, 500000, 1000000});
-    pop_first_bm.run("List", 1, popFirst<List<int>>, "Elements",
-                     {100, 1000, 10000, 50000, 100000, 500000, 1000000});
-    pop_first_bm.serialize("PopFirst", "popfirst.txt");
+    start = clock();
+    randomInsert<List<int>>(100000);
+    end = clock();
+    std::cout << "RandomInsert List: " << ((float) (end - start)) / CLOCKS_PER_SEC << std::endl;
 
-    std::cout << "Random" << std::endl;
-*/
-    bmk::benchmark<> random_insert_bm;
-    random_insert_bm.run("Vector", 1, randomInsert<Vector<int>>, "Elements",
-                         {100, 1000, 10000, 20000, 40000, 50000, 100000});
-    random_insert_bm.run("List", 1, randomInsert<List<int>>, "Elements",
-                         {100, 1000, 10000, 20000, 40000, 50000, 100000});
-    random_insert_bm.serialize("RandomInsert", "randominsert.txt");
 
     return 0;
 }
